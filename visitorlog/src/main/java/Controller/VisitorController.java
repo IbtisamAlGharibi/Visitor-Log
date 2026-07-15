eb04bf006d3725f0d3c07751ecbd649b5b294533
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-15 11:36:02 +0400

    some changes are made to fix the error

--- visitorlog/src/main/java/Controller/VisitorController.java
+++ visitorlog/src/main/java/Controller/VisitorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("visitor")
+@RequestMapping
 public class VisitorController {
     VisitorService visitorService;
     @Autowired
