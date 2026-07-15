1651c2badb5f7f23d98e3b3fd6737f9f369d6b50
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-15 09:28:59 +0400

    add and getVisitors methods is created

--- visitorlog/src/main/java/Service/VisitorService.java
+++ visitorlog/src/main/java/Service/VisitorService.java
@@ -1,7 +1,28 @@
 package Service;
 
+import Entity.Visitor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VisitorService {
+    List<Visitor> visitors = new ArrayList<>();
+
+    public Visitor add(Visitor visitor){
+        visitor.setId(visitor.getId());
+        visitor.setName(visitor.getName());
+        visitor.setCompany(visitor.getCompany());
+        visitor.setPurpose(visitor.getPurpose());
+        visitors.add(visitor);
+        return visitor;
+    }
+
+    public List<Visitor> getVisitors(){
+        for (Visitor visitor : visitors){
+            visitors.add(visitor);
+        }
+        return visitors;
+    }
 }
