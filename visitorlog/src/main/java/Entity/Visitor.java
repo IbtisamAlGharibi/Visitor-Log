96e8bcaf82ba4140e47e564c40a8d2e0ee831041
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-15 08:50:04 +0400

    Visitor class is added

---
+++ visitorlog/src/main/java/Entity/Visitor.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visitor {
+    private Long id;
+    private String name;
+    private String company;
+    private String purpose;
+
+}
